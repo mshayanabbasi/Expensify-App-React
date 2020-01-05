76c63ff0451278e24999525b4beb571cf473066d
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-01-05 12:02:45 +0500

    expensify

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -19,30 +19,32 @@ const resetCount = () => ({
     type: 'RESET'
 })
 
+// Reducers
 
-const store = createStore((state = { count: 0 }, action) => {
+const countReducer = (state = {count : 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
                 count: state.count + action.incrementBy
             }
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             }
         case 'RESET':
             return {
                 count: 0
-            }
-        case 'SET': 
+            }    
+        case 'SET':
             return {
                 count: action.count
             }
         default:
             return state
     }
-})
+}
+
+const store = createStore(countReducer)
 
 const unsubscribe =  store.subscribe(() => {
     console.log(store.getState())
