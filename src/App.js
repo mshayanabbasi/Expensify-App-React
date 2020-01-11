8da607a27efcf505546007fa7dd8a960ca8cbf29
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-01-11 14:14:57 +0500

    expensify

--- src/App.js
+++ src/App.js
@@ -6,20 +6,9 @@ import 'normalize.css/normalize.css'
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
-import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore()
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
-// store.dispatch(setTextFilter('water'))
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'))
-// })
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
