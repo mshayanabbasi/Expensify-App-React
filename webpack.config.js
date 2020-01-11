0d8ac69e74e9b7002dcce327af5376793dd0b32b
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-01-11 14:18:34 +0500

    expensify

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,42 @@
 const path = require('path')
- 
-module.exports = {
-    entry: './src/App.js', 
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    }, 
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    
+    return {
+        entry: './src/App.js', 
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        }, 
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\s?.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: true
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin()
+        ],
+        devtool: isProduction ? 'source-map' : 'initial-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 };
+ 
