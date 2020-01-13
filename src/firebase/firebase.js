81994e8cb09150ae0def7d403c50454fcbd805e1
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-01-13 22:49:35 +0500

    expensify

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,24 +13,85 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 const database = firebase.database()
-database.ref().set({
-    name: 'Andrew mead',
-    age: 26,
-    isSingle: false,
-    location: {
-        city: 'Philadelphia',
-        country: 'United States'
-    }
+
+database.ref().update({
+    name: 'Andrew'
 })
 
-// database.ref().set('This is my data.')
-
-database.ref('age').set(27)
-database.ref('location').set('New York')
-
-database.ref('attributes').set({
-    height: 73,
-    width: 150
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val()
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
 })
 
-console.log('I made a request to change the data.')
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val())
+// })
+// setTimeout(() => {
+//     database.ref('age').set(29)
+// }, 3500)
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange)
+// }, 7000)
+
+// setTimeout(() => {
+//     database.ref('age').set(30)
+// }, 10500)
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val()
+//         console.log(val)
+//     }).catch(error => {
+//         console.log('Error fetching data', error)
+//     })
+
+// database.ref().set({
+//     name: 'Andrew mead',
+//     age: 26,
+//     stressLevel: 6,
+//     isSingle: false,
+//     job: {
+//         title: 'Software Developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Philadelphia',
+//         country: 'United States'
+//     }
+// }).then(() => {
+//     console.log('Data is saved')
+// }).catch((e) => {
+//     console.log('This failed.', e)
+// })
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// })
+
+// // database.ref().set('This is my data.')
+
+// database.ref('age').set(27)
+// database.ref('location').set('New York')
+
+// database.ref('attributes').set({
+//     height: 73,
+//     width: 150
+// }).then(() => {
+//     console.log('Second set call worked.')
+// }).catch((e) => {
+//     console.log('Things didnt for the second error', e)
+// })
+
+// database.ref('isSingle').set(null)
+
+// database.ref('isSingle').remove()
+// .then(() => {
+//     console.log('Data was removed.')
+// }).catch((e) => {
+//     console.log('Data was not removed',e)
+// })
+
+// console.log('I made a request to change the data.')
