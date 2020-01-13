784ec1771035268a4a014c2915f1330173621f34
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-01-13 22:49:47 +0500

    expensify

---
+++ src/playground/promise.js
@@ -0,0 +1,15 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('this is my data.   ')
+    }, 5000)
+});
+
+console.log('before')
+
+promise.then((data) => {
+    console.log('1', data)
+}).catch((error) => {
+    console.log('error:', error)
+})
+
+console.log('after')
