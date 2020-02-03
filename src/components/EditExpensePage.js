14103f7b6aa48fd0e655bc40f47c989b225acf3d
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-02-03 21:32:29 +0500

    changes

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
-import { editExpense, removeExpense } from '../actions/expenses'
+import { editExpense, startRemoveExpense, startSetExpenses } from '../actions/expenses'
 
 const EditExpensePage = (props) => {
     console.log(props)
@@ -15,7 +15,7 @@ const EditExpensePage = (props) => {
                 }}
             />
             <button onClick={() => {
-                props.dispatch(removeExpense({id: props.expense.id}))
+                props.dispatch(startRemoveExpense({id: props.expense.id}))
                 props.history.push('/')
                 }}
             >Remove</button>
@@ -26,6 +26,13 @@ const EditExpensePage = (props) => {
 const mapStateToProps = (state, props) => {
     return {
         expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    }
+}
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+        startRemoveExpense: (data) => dispatch(startSetExpenses(data))
     }
 }
 
