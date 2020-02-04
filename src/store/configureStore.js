d1f3785e9cab502fe0fbd5835781d207c88389eb
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-02-05 00:22:30 +0500

    changes

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import expenseReducer from '../reducers/expenses'
 import filtersReducer from '../reducers/filters'
+import authReducer from '../reducers/auth'
 import thunk from 'redux-thunk'
 
 const composeEnchares = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose
@@ -9,7 +10,8 @@ export default () => {
     const store = createStore(
         combineReducers({
             expenses: expenseReducer,
-            filters: filtersReducer
+            filters: filtersReducer,
+            auth: authReducer
         }),
         composeEnchares(applyMiddleware(thunk))
         // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
