0333a16d7eff099d0e4f4dc145a188a5c594cfa7
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-02-05 22:25:01 +0500

    done

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import AppRouter, { history } from './routers/AppRouter'
 import { firebase } from './firebase/firebase'
 import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
-
+import LoadingPage from './components/LoadingPage'
 const store = configureStore()
 
 // const state = store.getState()
@@ -23,7 +23,7 @@ const jsx = (
     </Provider>
 )
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+ReactDOM.render(<LoadingPage />, document.getElementById('app'))
 
 let hasRendered = false
 const renderApp = () => {
